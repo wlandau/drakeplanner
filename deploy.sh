d35d0e1c59fca93d744e6671e22bbb52faebfdda
wlandau-lilly <will.landau@lilly.com>
2019-09-30 08:34:08 -0400

    Ensure no deployment if not PR

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+[ -z "${GITHUB_PAT}" ] && exit 0
+echo "Environment variables:"
+echo "  PATH: ${PATH}"
+echo "  TRAVIS_BRANCH: ${TRAVIS_BRANCH}"
+echo "  TRAVIS_PULL_REQUEST: ${TRAVIS_PULL_REQUEST}"
+echo "  TRAVIS_PULL_REQUEST_BRANCH: ${TRAVIS_PULL_REQUEST_BRANCH}"
+if [[ "${TRAVIS_BRANCH}" == "master" && "${TRAVIS_PULL_REQUEST}" == "false" ]]
+then
+  echo "Deploying to shinyapps.io."
+  Rscript shinyapps.R
+fi
