b450e738171f6e2f1327936ad0bbdf673ff868b1
wlandau-lilly <will.landau@lilly.com>
2019-09-30 08:24:46 -0400

    Use msgs

--- rsconnect.R
+++ rsconnect.R
@@ -1,12 +1,12 @@
 library(rsconnect)
 remotes::install_github("wlandau/drakeplanner")
-cat("Setting account info.")
+message("Setting account info.")
 setAccountInfo(
   name   = Sys.getenv("shinyapps_name"),
   token  = Sys.getenv("shinyapps_token"),
   secret = Sys.getenv("shinyapps_secret")
 )
-cat("Deploying to shinyapps.io.")
+message("Deploying to shinyapps.io.")
 deployApp(
   appDir = file.path("tests", "apps", "drakeplanner"),
   appName = "drakeplanner"
